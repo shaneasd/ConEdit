cdc959c1cfc0671e0d3a6ad0738deda6cd84ba86
shane <thatguyiknow5@hotmail.com>
2017-04-22 02:09:03 +0800

    Add BottleNeck extension method for ensuring Funcs do not run in parallel

---
+++ Utilities/BottleNeck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class BottleNeckUtil
+    {
+        public static Func<T, Result> BottleNeck<T, Result>(this Func<T, Result> function)
+        {
+            object sync = new object();
+            return x =>
+            {
+                lock (sync)
+                {
+                    return function(x);
+                }
+            };
+        }
+    }
+}
